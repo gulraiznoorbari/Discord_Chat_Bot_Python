d3e915d34b85094f46632362450287283228b3e0
gulraiz <gulraiznoorbari@gmail.com>
2023-02-08 17:07:26 +0500

    Fixed on_member_join event

--- cogs/members.py
+++ cogs/members.py
@@ -8,10 +8,18 @@ class members(commands.Cog):
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
-        guild = await self.bot.get_guild(1065617002393247808)
-        general_channel = await self.bot.fetch_channel(1065617003949338676)
-        await general_channel.send(f"Hey {member.mention}! \n\n\U0001F389\U0001F389WELCOME TO THE SERVER\U0001F389\U0001F389\n\nThis is <#{general_channel}> channel.")
-        await member.send(f"Welcome to the {guild.name} Discord Server \n Use ```!help``` to see the list of commands I can do!")
+        guild_name = member.guild.name
+        print("Server Name: ", guild_name)
+
+        channel = discord.utils.get(
+            member.guild.channels,
+            name="general"
+        )
+        channel_id = channel.id
+        print("Channel Id: ", channel)
+
+        await channel.send(f"Hey {member.mention}! \n\n\U0001F389\U0001F389WELCOME TO THE SERVER\U0001F389\U0001F389\n\n")
+        await member.send(f"Welcome to the {guild_name} Discord Server \nUse ***'!help'*** in <#{channel_id}> to see the list of commands I can do!")
 
     @commands.Cog.listener()
     async def on_member_remove(self, member):
